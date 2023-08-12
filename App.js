09a0456d7f1229541eabf1c35e831667d6fd9f48
Gerald-Z <geraldjialeizhu@gmail.com>
2023-08-12 17:26:20 -0500

    gitignore update

--- App.js
+++ App.js
@@ -5,3 +5,48 @@ const mongoose = require("mongoose");
 
 // Connecting to the database
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+
+const positionSchema = new Schema({
+    CompanyName: String,
+    Ticker: String,
+    SharesOwned: Number,
+    SharePrice: Number,
+    TotalCost: Number,
+    TotalValue: Number,
+    EstimatedDivPayout: Number,
+    EstimatedDivYield: Number
+  });
+
+const Position = mongoose.model("Position", positionSchema);
+
+
+const createAndSavePosition = () => {
+    var newPos = new Position({
+        CompanyName: "JP Morgan",
+        Ticker: "JPM",
+        SharesOwned: 100,
+        SharePrice: 100,
+        TotalCost: 10000,
+        TotalValue: 10000,
+        EstimatedDivPayout: 100,
+        EstimatedDivYield: 100
+    });
+    newPos.save().then(saved => {
+        saved === newPos; // true
+    });
+};
+
+const finished = function(err, data) {
+    if (err) {
+        console.log("There has been an error");
+        console.log(err);
+        return;
+    } else {
+        console.log("The code is done");
+        console.log(data);
+    }
+}
+
+createAndSavePosition();
